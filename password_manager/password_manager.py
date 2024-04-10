bd95e221a5548c084ea9c935f3bae118a0544d30
sdale0321 <67200326+sdale0321@users.noreply.github.com>
2024-04-09 21:05:30 -0500

    Add files via upload
    In progress password manager

---
+++ password_manager/password_manager.py
@@ -0,0 +1,26 @@
+master_pwd = input("What is the master password? ")
+
+def view():
+    pass
+
+def add():
+    name = input('Account Name: ')
+    pwd = input("Password: ")
+
+    with open('passwords.txt', 'a') as f:
+        f.write(name + "|" + pwd)
+
+
+while True:
+    mode = input("Would you like to add a new password or view existing ones (add/view), press q to quit? ")
+
+    if mode == "q": 
+        break
+
+    if mode == "view": 
+        view()
+    elif mode == "add":
+        add()
+    else: 
+        print("Invalid mode.")
+        continue
